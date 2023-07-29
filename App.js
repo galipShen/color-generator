4e348bd447536785803d916f82d068357c1e008f
Galip Sen <galipshen@gmail.com>
2023-07-29 12:14:53 +0300

    4 mumdur

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>üç mumdur </Text>
+      <Text>4 mumdur </Text>
     </View>
   )
 };
