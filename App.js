9f534bb54665f308b8394425cb75c2e59866f833
Galip Sen <galipshen@gmail.com>
2023-07-29 16:08:12 +0300

    all files cleared to default

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>4 mumdur </Text>
+      <Text>brand new </Text>
     </View>
   )
 };
